311948097d1aa0a5783104ac45f72b380c3044c7
Sandeep Singh Bisht <sandeepsingh786687@gmail.com>
2026-01-21 11:41:05 +0530

    Burder_newsite_suman

---
+++ script.js
@@ -0,0 +1,14 @@
+ 
+  const currentPage = window.location.pathname.split("/").pop();
+  const navLinks = document.querySelectorAll(".nav-link");
+
+  navLinks.forEach(link => {
+    const linkPage = link.getAttribute("href");
+
+    if (linkPage === currentPage || (currentPage === "" && linkPage === "index.html")) {
+      link.classList.add("active");
+    } else {
+      link.classList.remove("active");
+    }
+  });
+ 
